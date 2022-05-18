7a9ba0988ee766dffbf074ebb6c38ef897914222
Liam Mueller <liam.mueller315@gmail.com>
2022-05-17 20:18:37 -0500

    Map ctrl+h to hover definition.

--- vim.nix
+++ vim.nix
@@ -88,12 +88,15 @@ let
         else
           inoremap <silent><expr> <c-@> coc#refresh()
         endif
-
+ 
         " Remap keys for gotos
         nmap <silent> gd <Plug>(coc-definition)
         nmap <silent> gy <Plug>(coc-type-definition)
         nmap <silent> gi <Plug>(coc-implementation)
         nmap <silent> gr <Plug>(coc-references)
+
+        " Mapping for hovering definition
+        noremap <silent> <C-h> :call CocActionAsync('doHover')<CR>
 
         " ALE settings.
         let g:airline#extensions#ale#enabled = 1
